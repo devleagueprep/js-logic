b631abcdc45daf277f37c4f0f31bf836cdadf57d
Bryan Choe <bryan.choe88@gmail.com>
2017-02-08 19:32:37 -1000

    changes

--- random.js
+++ random.js
@@ -1,4 +1,16 @@
-var students = ['Christine', 'Jacob', 'Anthony', 'Earnest', 'Ann', 'Ella', 'James', 'Joshua', 'Kaleo', 'Ulu', 'Gaganvir'];
+var students = [
+  'Christine',
+  'Jacob',
+  'Anthony',
+  'Earnest',
+  'Ann',
+  'Ella',
+  'James',
+  'Joshua',
+  'Kaleo',
+  'Ulu',
+  'Gaganvir'
+  ];
 
 var randomIndex = Math.floor(Math.random() * students.length);
 var randomElement = students[randomIndex];
