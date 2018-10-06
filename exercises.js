dac06ffbf5b0f260cdf71dffd7423ece95dc1f23
madycakes <viclee02@gmail.com>
2018-10-05 21:18:57 -1000

    complete ex1

--- exercises.js
+++ exercises.js
@@ -29,11 +29,14 @@ If/else statements = Evaluates (or checks) a condition. If the condition is true
  *
  * The function will return true if the number passed into the function is equal to or greater than Hawaii's voting age. Console.log your result.
 */
+function canVote(age){
+  if(age >= 18){
+    return true;
+  }
 
-function canVote(){
-
-  
 }
+
+console.log('can vote results', canVote(18))
 
 
 /*
